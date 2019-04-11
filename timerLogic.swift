a7ebcd38c5a6199266063f7ee9f6500bd43f7bdc
Kate Kearney <katekearney27@gmail.com>
2019-04-11 12:14:04 -0500

    Initial commit

---
+++ timerLogic.swift
@@ -0,0 +1,13 @@
+//
+//  timerLogic.swift
+//  TaskBubble
+//
+//  Created by Kate Kearney on 4/11/19.
+//  Copyright © 2019 Kate Kearney. All rights reserved.
+//
+
+import Foundation
+
+struct timerLogic{
+    
+}
