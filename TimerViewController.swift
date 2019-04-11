e097909ba85374a7d08fa70362071dcc14cd166d
Kate Kearney <katekearney27@gmail.com>
2019-04-11 12:52:16 -0500

    I'm still having issuers with connecting labels and buttons to the view controller :(

---
+++ TimerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TimerViewController.swift
+//  TaskBubble
+//
+//  Created by Kate Kearney on 4/11/19.
+//  Copyright © 2019 Kate Kearney. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
