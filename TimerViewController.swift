4dae9a62277ca64172b9574d6143b3268ce13382
Kate Kearney <katekearney27@gmail.com>
2019-04-11 12:59:13 -0500

    update

--- TimerViewController.swift
+++ TimerViewController.swift
@@ -10,7 +10,34 @@ import UIKit
 
 class TimerViewController: UIViewController {
     
+    
+    
+    var clock = timerLogic()
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    var resumeTapped = false
+    
+    
+    @objc func updateTimer() {
+        seconds -= 1
+        timerLabel.text = String(seconds)
+    }
+    
+    func runTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(timerView.updateTimer)), userInfo: nil, repeats: true)
+    }
 
+    @IBAction func pauseButton(_ sender: Any) {
+        if self.resumeTapped == false {
+            timer.invalidate()
+            self.resumeTapped = true
+        } else {
+            runTimer()
+            self.resumeTapped = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
